732ab95c041700f978b248bc3f80f9f80f9d0fb5
ktakuminnnjp1983 <ktakuminnnjp1983@gmail.com>
2014-01-21 19:00:37 +0900

    test

--- helloworld/public/javascripts/client.js
+++ helloworld/public/javascripts/client.js
@@ -1,4 +1,4 @@
-var socket = new WebSocket("ws://192.168.11.10:3000");
+var socket = new WebSocket("ws://172.27.66.36:3000");
 socket.binaryType = "blob";
 
 socket.onopen = function(){
@@ -6,29 +6,35 @@ socket.onopen = function(){
 };
 
 socket.onmessage = function(message){
+    console.log(message.constructor);
     console.log(message);
+    console.log(message.data);
     var reader = new FileReader();
     reader.onload = function(e){
         console.log("readblobdata");
         $("#imgArea").attr("src", reader.result);
     }
     reader.readAsDataURL(message.data);
+    
+    // var url = URL.createObjectURL(message.data);
+    // console.log(url);
+    // $("#imgArea").attr("src", url);
 }
 
-var mediaStream;
-navigator.webkitGetUserMedia(
-    {video: true, audio: true},
-    function(stream){
-        mediaStream = stream;
-        for(var prop in stream){
-                console.log(prop + " " + stream[prop]);
-        }
-        alert(stream.record);
-    }, 
-    function(){
-        alert("media fail");
-    }
-);
+// var mediaStream;
+// navigator.webkitGetUserMedia(
+    // {video: true, audio: true},
+    // function(stream){
+        // mediaStream = stream;
+        // for(var prop in stream){
+                // console.log(prop + " " + stream[prop]);
+        // }
+        // alert(stream.record);
+    // }, 
+    // function(){
+        // alert("media fail");
+    // }
+// );
 
 
 $(function(){
@@ -39,12 +45,13 @@ $(function(){
     $("#fileButton").change(function(event){
         var f = $("#fileButton").get(0).files[0];
         socket.send(f);
+        // FileReaderを使ってarrayBufferを送ってもよい
     });
 
-    setTimeout(function(){
-        var recorder = mediaStream.record();
-        recorder.getRecordedData(function(blob){
-           socket.send(blob);
-        });
-    }, 1000);
+    // setTimeout(function(){
+        // var recorder = mediaStream.record();
+        // recorder.getRecordedData(function(blob){
+           // socket.send(blob);
+        // });
+    // }, 1000);
 });
